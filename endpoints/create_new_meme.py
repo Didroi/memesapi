65b7645d5fa870e5224ec04f12d28bad4f3bb6a0
Dmitrii Kiselev <didroi@yandex.ru>
2024-07-25 20:54:44 +0200

    one_test_doesnt_work

--- endpoints/create_new_meme.py
+++ endpoints/create_new_meme.py
@@ -19,6 +19,26 @@ class MemeCreator(BaseApi):
         self.header = header
         return self.response_json['id']
 
+    @allure.step('Creating new meme')
+    def create_meme_without_json(self, header, payload):
+
+        self.response = requests.post(
+            f'{url.url}/meme',
+            json=payload,
+            headers=header
+        )
+
+    @allure.step('Creating new meme')
+    def create_meme_with_different_data(self, header, original_payload, field, data):
+        payload = original_payload.copy()
+        payload[field] = data
+
+        self.response = requests.post(
+            f'{url.url}/meme',
+            json=payload,
+            headers=header
+        )
+
     def check_id_in_response(self):
         print(self.response_json['id'])
         return self.response_json['id'] > 0
@@ -26,11 +46,3 @@ class MemeCreator(BaseApi):
     def delete_meme_after_creating(self):
         delete_obj = MemeAnnihilator()
         delete_obj.delete_meme(self.header, self.response_json['id'])
-    #
-    # @allure.step('Fetch single meme by ID')
-    # def fetch_meme_by_id_without_json(self, header, id):
-    #
-    #     self.response = requests.get(
-    #         f'{url.url}/meme/{id}',
-    #         headers=header
-    #     )
--- tests/data/payloads.py
+++ tests/data/payloads.py
@@ -2,6 +2,11 @@ token_payload = {
     "name": "dmitrii_k"
 }
 
+another_token_payload = {
+    "name": "lzhe_dmitrii!"
+}
+
+
 meme_payload = {
     "text": "Difference",
     "tags": [
@@ -12,3 +17,28 @@ meme_payload = {
     "url": "https://global.discourse-cdn.com/business5/uploads/gemsofwar/original/3X/0/e/0ec8397b1ea72bfbf205839b913d3ad2a63df70a.jpeg",
     "info": {}
 }
+
+empty_meme_payload = {
+    "text": "",
+    "tags": [],
+    "url": "",
+    "info": {}
+}
+
+payload_without_field = [{
+    "tags": [],
+    "url": "",
+    "info": {}
+}, {
+    "text": "",
+    "url": "",
+    "info": {}
+}, {
+    "text": "",
+    "tags": [],
+    "info": {}
+}, {
+    "text": "",
+    "tags": [],
+    "url": ""
+}]
